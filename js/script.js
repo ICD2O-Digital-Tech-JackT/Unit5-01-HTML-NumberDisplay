c19c8306593f62272caeefa0701756f0ba416408
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-05-27 14:38:28 +0000

    Finished JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+function Submit(){
+  let min = document.getElementById('min').value;
+  let max = document.getElementById('max').value;
+  if (min&&max){
+    if (Number(min)<Number(max)){
+      let result = "";
+      for (let i=Number(min); i<=Number(max); i++){
+        result += i + " ";
+      }
+      document.getElementById('Result').innerHTML = result;
+    } else{
+      document.getElementById('Result').innerHTML = "Please enter a min number that is less than your max number";
+      document.getElementById('Result').innerHTML =min+" "+max
+    }
+  } else{
+    document.getElementById('Result').innerHTML = "Please enter 2 valid numbers";
+  }
+}
